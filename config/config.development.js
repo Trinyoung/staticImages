7fb6d73ee6137f3fa40f2f8baa60902c7db0fceb
trinyoung <trinyoung@163.com>
2021-03-29 11:04:04 +0800

    fix(config): 修改文件

--- config/config.development.js
+++ config/config.development.js
@@ -1,13 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2020-12-21 09:06:37
- * @LastEditTime: 2020-12-21 09:51:37
+ * @LastEditTime: 2021-03-29 11:02:59
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \staticImages\config\config.dev.js
  */
 const path = require('path')
 module.exports = {
-    resBaseUrl: '',
-    storeUrl: path.join(__dirname, '/static')
+    resBaseUrl: 'http://localhost:3000/uploads',
+    storeUrl: path.join(__dirname, '../static/uploads')
 };
